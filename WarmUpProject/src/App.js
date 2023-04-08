07a20cdf425eb230608c8f997bc53878038d3b47
dandamdandam <glue0440@gmail.com>
2023-04-08 21:28:54 +0900

    [Feat] #3 - Add homepage(Home.js)

--- WarmUpProject/src/App.js
+++ WarmUpProject/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Home from './Home';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
+        <Route path={"/"} element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ WarmUpProject/src/Home.js
@@ -0,0 +1,18 @@
+//홈 페이지
+
+function Home(){
+    
+    return (
+        <div className="Home">
+            <h1>
+                <span id='homeH1_1'>Pikachu</span>
+                <span id='homeH1_2'>Coding</span>
+            </h1>
+            <h2>팀원 소개 둘러보기</h2>
+            {/* movePer 컴포넌트로 개인 탭으로 이동하는 카드 추가하기 */}
+            <h2>팀 소개</h2>
+        </div>
+    );
+}
+
+export default Home;
