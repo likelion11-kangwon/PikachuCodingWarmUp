ca0ebb856320d818edc625d17c6ca9c391108908
dandamdandam <glue0440@gmail.com>
2023-04-08 21:09:49 +0900

    [Feat] #2 - Add icon

--- WarmUpProject/src/index.js
+++ WarmUpProject/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
+<link rel="icon" href="%PUBLIC_URL%/pika_icon.ico" />
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
