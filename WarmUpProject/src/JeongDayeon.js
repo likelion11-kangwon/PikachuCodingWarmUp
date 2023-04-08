c6953d93600d6ee4ed5c21b4dcc7349b455f5806
dandamdandam <glue0440@gmail.com>
2023-04-08 21:50:13 +0900

    [Feat] #4 - Update content to JDY
    머릿말, about me 추가(Profile, AboutMe 컴포넌트)

--- WarmUpProject/src/JeongDayeon.js
+++ WarmUpProject/src/JeongDayeon.js
@@ -3,7 +3,58 @@
 function JeongDayeon() {
   return (
     <div className='JeongDayeon'>
+        <Profile />
+        <AboutMe />
     </div>
+  );
+}
+
+function Profile(){
+  return (
+    <div className="content" id="profile">
+      <header>
+        {/* 캐로셀 테스트용
+        <img src="images/headimgtest.png"> */}
+        <h1>
+            안녕하세요
+            <br/>이것저것 도전 중!
+            <br/><span className='colored-text'>정다연</span>입니다.
+        </h1>
+        <div className="article">
+            <p className='small_font'>
+              2004.01.16
+              <br/>컴퓨터공학과 22학번
+              <br/>Infj &#40;i 빼고 다 소문자입니다&#41;
+            </p>
+            <p>
+                대학교 입학하면서 프로그래밍 프로젝트에 관심을 갖게 되어 이것저것 공부하고 있습니다. 아직까진 코딩이 재밌고 짜릿합니다.
+            </p>
+            <p>
+                <span className="small_font">
+                협업경험이라곤 작년 학교에서 진행한 아이컨택 부트캠프, 친구와 소소한 유니티 만지기, 지금하고 있는 프로젝트 밖엔 없지만..
+                </span>
+                <br/>팀프로젝트하면서 짜릿함을 느낍니다! 
+                고로 누구보다도 진심으로 참여해 팀원들과 멋진 결과물을 만들어나갈 자신 있습니다.
+            </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+function AboutMe(){
+  return(
+    <div className="content" id="about_me">
+      <h2>More about me</h2>
+      <p>관심분야: 웹, 인공지능, 데이터</p>
+      <p>
+        😽 📖 🎤 🏀 💻
+        <span className='small_font'>
+        <br/>고양이에 돌아있고, 헛되더라도 코드 짜는 것에 빠져있으며, 영화*책*음악(노래) 장르 안 가리고 다 좋아합니다. 
+        특히 마지막은 추천도 받습니다! 관련 얘기하면서 친해지고 싶으면 언제든 연락주세요!
+        </span>
+      </p>
+      <p>현재 풀스택이라는 단어에 꽂혀서, 웹 a-z를 목표로 공부하고 있습니다.</p>
+    </div>  
   );
 }
 
