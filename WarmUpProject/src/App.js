d83dce59253f41daff0f2b768754db20f12cccca
dandamdandam <glue0440@gmail.com>
2023-04-17 23:46:35 +0900

    [Feat] #4 - Route personal pages
    home에 각자의 개인 페이지 연결

--- WarmUpProject/src/App.js
+++ WarmUpProject/src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HanKyungjun from './HanKyungjun';
+import KimSeongbin from './KimSeongbin';
+import SeoSangtae from './SeoSangtae';
 import JeongDayeon from './JeongDayeon';
 import Home from './Home';
 
@@ -8,6 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={"/"} element={<Home />}></Route>
+          <Route path={"/HanKyungjun"} element={<HanKyungjun/>}></Route>
+          <Route path={"/KimSeongbin"} element={<KimSeongbin/>}></Route>
+          <Route path={"/SeoSangtae"} element={<SeoSangtae/>}></Route>
           <Route path={"/JeongDayeon"} element={<JeongDayeon />}></Route>
         </Routes>
       </BrowserRouter>
--- WarmUpProject/src/Home.js
+++ WarmUpProject/src/Home.js
@@ -11,7 +11,10 @@ function Home(){
             </h1>
             <h2>팀원 소개 둘러보기</h2>
             {/* movePer 컴포넌트로 개인 탭으로 이동하는 카드 추가하기 */}
-            <MovePer img_path="" page_path="/JeongDayeon" name="정다연" num="컴공22"/>
+            <MovePer img_path="" page_path="/HanKyungjun" name="한경준" num="컴공 23"/>
+            <MovePer img_path="" page_path="/KimSeongbin" name="김성빈" num="AI 20"/>
+            <MovePer img_path="" page_path="/SeoSangtae" name="서상태" num="산공 19"/>
+            <MovePer img_path="" page_path="/JeongDayeon" name="정다연" num="컴공 22"/>
             <h2>팀 소개</h2>
         </div>
     );
